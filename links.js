476c423520fc956605901605a73d5e93e68eda9c
Rahul Nair <rnair12599@gmail.com>
2020-10-18 01:57:23 -0700

    added first commit

---
+++ links.js
@@ -0,0 +1,18 @@
+const linksArr = [
+    { 
+        "name" : "Youtube",
+        "url": "https://www.youtube.com/",
+    },
+
+    { 
+        "name" : "Google",
+        "url": "https://www.google.com/",
+    },
+
+    { 
+        "name" : "Facebook",
+        "url": "https://www.facebook.com/",
+    },
+
+
+];
